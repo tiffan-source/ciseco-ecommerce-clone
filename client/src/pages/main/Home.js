7ec382f63db7decaa282203ec79bfaf32c7a8992
hasibulislam999 <hasib143sl@gmail.com>
2023-03-31 11:03:24 +0600

    init home page class names

--- client/src/pages/main/Home.js
+++ client/src/pages/main/Home.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Banner from "../../components/home/Banner";
+import BuyingSteps from "../../components/home/BuyingSteps";
 
 const Home = () => {
-  return <>
-    <Banner />
-  </>;
+  return (
+    <section className="container mx-auto flex flex-col gap-y-32 px-4">
+      <Banner />
+      <BuyingSteps />
+    </section>
+  );
 };
 
 export default Home;
