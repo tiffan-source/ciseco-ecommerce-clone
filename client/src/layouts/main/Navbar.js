bd99cc41a13a6098cbbfa311c10e3eb13f71fc48
hasibulislam999 <hasib143sl@gmail.com>
2023-04-01 18:09:28 +0600

    link with profile dropdown

--- client/src/layouts/main/Navbar.js
+++ client/src/layouts/main/Navbar.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import Dropdown from "../../components/main/Dropdown";
+import Profile from "../../components/main/Profile";
 
 export default function NavBar() {
   const [navbar, setNavbar] = useState(false);
@@ -112,30 +113,7 @@ export default function NavBar() {
                 </Link>
               </li>
               <li className="text-gray-600 hover:text-blue-600">
-                <Link to="/" className="flex gap-x-4 items-center">
-                  <svg
-                    className=" w-6 h-6"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M12 12C14.7614 12 17 9.76142 17 7C17 4.23858 14.7614 2 12 2C9.23858 2 7 4.23858 7 7C7 9.76142 9.23858 12 12 12Z"
-                      stroke="currentColor"
-                      strokeWidth="1.5"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    ></path>
-                    <path
-                      d="M20.5899 22C20.5899 18.13 16.7399 15 11.9999 15C7.25991 15 3.40991 18.13 3.40991 22"
-                      stroke="currentColor"
-                      strokeWidth="1.5"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    ></path>
-                  </svg>
-                  <span className="block md:hidden">Profile</span>
-                </Link>
+                <Profile />
               </li>
             </ul>
           </div>
