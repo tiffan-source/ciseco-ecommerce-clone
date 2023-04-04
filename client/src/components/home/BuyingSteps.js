d42ce43fc2eed7df2f32f1a90669b0c4461d5bde
hasibulislam999 <hasib143sl@gmail.com>
2023-04-04 18:07:45 +0600

    buying step modify by changing class to className

--- client/src/components/home/BuyingSteps.js
+++ client/src/components/home/BuyingSteps.js
@@ -4,7 +4,7 @@ const BuyingSteps = () => {
   const steps = [
     {
       badge: (
-        <span class="inline-flex px-2.5 py-1 rounded-full font-medium text-xs text-red-800 bg-red-100 relative">
+        <span className="inline-flex px-2.5 py-1 rounded-full font-medium text-xs text-red-800 bg-red-100 relative">
           Step 1
         </span>
       ),
@@ -14,7 +14,7 @@ const BuyingSteps = () => {
     },
     {
       badge: (
-        <span class="nc-Badge inline-flex px-2.5 py-1 rounded-full font-medium text-xs text-indigo-800 bg-indigo-100 relative">
+        <span className="nc-Badge inline-flex px-2.5 py-1 rounded-full font-medium text-xs text-indigo-800 bg-indigo-100 relative">
           Step 2
         </span>
       ),
@@ -24,7 +24,7 @@ const BuyingSteps = () => {
     },
     {
       badge: (
-        <span class="nc-Badge inline-flex px-2.5 py-1 rounded-full font-medium text-xs text-yellow-800 bg-yellow-100 relative">
+        <span className="nc-Badge inline-flex px-2.5 py-1 rounded-full font-medium text-xs text-yellow-800 bg-yellow-100 relative">
           Step 3
         </span>
       ),
@@ -34,7 +34,7 @@ const BuyingSteps = () => {
     },
     {
       badge: (
-        <span class="nc-Badge inline-flex px-2.5 py-1 rounded-full font-medium text-xs text-purple-800 bg-purple-100 relative">
+        <span className="nc-Badge inline-flex px-2.5 py-1 rounded-full font-medium text-xs text-purple-800 bg-purple-100 relative">
           Step 4
         </span>
       ),
@@ -46,32 +46,32 @@ const BuyingSteps = () => {
 
   return (
     <section className="lg:px-32">
-      <div class="relative grid sm:grid-cols-2 lg:grid-cols-4 gap-10 sm:gap-16 xl:gap-20">
+      <div className="relative grid sm:grid-cols-2 lg:grid-cols-4 gap-10 sm:gap-16 xl:gap-20">
         <img
-          class="hidden md:block absolute inset-x-0 top-5"
+          className="hidden md:block absolute inset-x-0 top-5"
           src="/assets/steps/step-bg.svg"
           alt="vector"
         />
         {steps.map((step, index) => (
           <div
             key={index}
-            class="relative flex flex-col items-center max-w-xs mx-auto"
+            className="relative flex flex-col items-center max-w-xs mx-auto"
           >
             <div
-              class="nc-NcImage mb-4 sm:mb-10 max-w-[140px] mx-auto"
+              className="nc-NcImage mb-4 sm:mb-10 max-w-[140px] mx-auto"
               data-nc-id="NcImage"
             >
               <img
                 src={step.thumbnail}
-                class="rounded-3xl"
+                className="rounded-3xl"
                 alt={step.title}
                 loading="lazy"
               />
             </div>
-            <div class="text-center mt-auto space-y-5">
+            <div className="text-center mt-auto space-y-5">
               {step.badge}
-              <h3 class="text-base font-semibold">{step.title}</h3>
-              <span class="block text-slate-600 dark:text-slate-400 text-sm leading-6">
+              <h3 className="text-base font-semibold">{step.title}</h3>
+              <span className="block text-slate-600 dark:text-slate-400 text-sm leading-6">
                 {step.description}
               </span>
             </div>
