040baf8800a8b459dd60780afc39dec553985a74
hasibulislam999 <hasib143sl@gmail.com>
2023-04-06 12:56:21 +0600

    integrate toast for alert poppers

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { RouterProvider } from "react-router-dom";
 import routes from "./routes/routes";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
     <>
       <RouterProvider router={routes} />
+      <Toaster position="top-center" reverseOrder={true} />
     </>
   );
 }
