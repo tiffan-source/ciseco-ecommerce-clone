cb7b117bd144edd860bf49e9719db903913eddb1
hasibulislam999 <hasib143sl@gmail.com>
2023-04-06 12:55:22 +0600

    init site footer

--- client/src/layouts/main/Footer.js
+++ client/src/layouts/main/Footer.js
@@ -7,11 +7,17 @@ const Footer = () => {
 
   return (
     <>
-      <footer class="bg-white shadow py-8">
+      <footer className="bg-white shadow py-8">
         <div className="container mx-auto">
           <div className="flex md:flex-row md:justify-between flex-col items-center md:gap-0 gap-y-4">
             <Link to="/" title="Ciseco ECommerce">
-              <img src="logo.png" alt="logo" loading="lazy" height="150" width="150" />
+              <img
+                src="logo.png"
+                alt="logo"
+                loading="lazy"
+                height="150"
+                width="150"
+              />
             </Link>
             <p>© {year} Ciseco ECommerce, All Right Reserved.</p>
           </div>
