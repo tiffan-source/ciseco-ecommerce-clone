c8671f2509bfc1dfa2003da7e5ecfbec240a5750
hasibulislam999 <hasib143sl@gmail.com>
2023-04-07 09:43:27 +0600

    integrate auth into redux store

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import apiSlice from "../features/api/apiSlice";
 import uploadSlice from "../features/upload/uploadSlice";
+import authSlice from "../features/auth/authSlice";
 
 const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
     upload: uploadSlice,
+    auth: authSlice,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(apiSlice.middleware),
