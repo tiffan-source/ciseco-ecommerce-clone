7a553c2f390834d1f509d2e51a25fbb21f5cc9ca
hasibulislam999 <hasib143sl@gmail.com>
2023-04-08 21:38:53 +0600

    keyword remodify

--- client/src/layouts/dashboard/Sidebar.js
+++ client/src/layouts/dashboard/Sidebar.js
@@ -64,7 +64,7 @@ function HiChartPie(props) {
       width="16"
       height="16"
       fill="currentColor"
-      class="bi bi-pie-chart-fill"
+      className="bi bi-pie-chart-fill"
       viewBox="0 0 16 16"
     >
       <path d="M15.985 8.5H8.207l-5.5 5.5a8 8 0 0 0 13.277-5.5zM2 13.292A8 8 0 0 1 7.5.015v7.778l-5.5 5.5zM8.5.015V7.5h7.485A8.001 8.001 0 0 0 8.5.015z" />
@@ -80,7 +80,7 @@ function HiHome(props) {
       width="16"
       height="16"
       fill="currentColor"
-      class="bi bi-house-door"
+      className="bi bi-house-door"
       viewBox="0 0 16 16"
     >
       <path d="M8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4.5a.5.5 0 0 0 .5-.5v-4h2v4a.5.5 0 0 0 .5.5H14a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146ZM2.5 14V7.707l5.5-5.5 5.5 5.5V14H10v-4a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v4H2.5Z" />
