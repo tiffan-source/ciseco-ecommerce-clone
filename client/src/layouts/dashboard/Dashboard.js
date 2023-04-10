9b8e91c3d1fda029b43e6cc3981ea08d81f1c08f
hasibulislam999 <hasib143sl@gmail.com>
2023-04-10 09:46:37 +0600

    board span refactored

--- client/src/layouts/dashboard/Dashboard.js
+++ client/src/layouts/dashboard/Dashboard.js
@@ -8,7 +8,7 @@ const Dashboard = () => {
       <section className="h-screen w-screen overflow-x-hidden overflow-y-scroll">
         <section className="grid grid-cols-12 p-4 gap-3 w-full h-full">
           <Sidebar />
-          <div className="lg:col-span-10 md:col-span-10 col-span-12 bg-gray-100 rounded-lg p-4 overflow-x-hidden overflow-y-scroll w-full h-full shadow">
+          <div className="lg:col-span-10 md:col-span-11 col-span-12 bg-gray-100 rounded-lg p-4 overflow-x-hidden overflow-y-scroll w-full h-full shadow">
             <Outlet />
           </div>
         </section>
