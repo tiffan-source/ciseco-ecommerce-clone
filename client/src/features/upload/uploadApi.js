c916bcf960793e752ffd17276ed6762110e0d3a3
hasibulislam999 <hasib143sl@gmail.com>
2023-04-14 22:09:31 +0600

    integrate dynamic update api

--- client/src/features/upload/uploadApi.js
+++ client/src/features/upload/uploadApi.js
@@ -1,26 +1,57 @@
 import apiSlice from "../api/apiSlice";
-import { setUpload } from "./uploadSlice";
+import { setGallery, setPhoto } from "./uploadSlice";
 
 const uploadApi = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
-    uploadUserAvatar: builder.mutation({
+    // single photo upload
+    uploadPhoto: builder.mutation({
+      query: ({ route, photo }) => ({
+        url: `api/${route}`,
+        method: "POST",
+        body: photo,
+      }),
+      invalidatesTags: ["Photo"],
+      async onQueryStarted(_, { dispatch, queryFulfilled }) {
+        try {
+          const photoData = await queryFulfilled;
+          photoData.data.acknowledgement
+            ? dispatch(
+                setPhoto({
+                  url: photoData.data.data.path,
+                  public_id: photoData.data.data.filename,
+                })
+              )
+            : console.log(photoData);
+        } catch (error) {
+          console.log(error);
+        }
+      },
+    }),
+
+    // gallery photos upload
+    uploadGallery: builder.mutation({
       query: (data) => ({
-        url: `api/user/avatar`,
+        url: `api/product/gallery`,
         method: "POST",
         body: data,
       }),
       invalidatesTags: ["Photo"],
       async onQueryStarted(_, { dispatch, queryFulfilled }) {
         try {
-          const avatarData = await queryFulfilled;
-          avatarData.data.acknowledgement
-            ? dispatch(
-                setUpload({
-                  url: avatarData.data.data.path,
-                  public_id: avatarData.data.data.filename,
-                })
-              )
-            : console.log(avatarData);
+          const galleryData = await queryFulfilled;
+
+          const galleryInformation = [];
+
+          for (let i = 0; i < galleryData.data.data.length; i++) {
+            galleryInformation.push({
+              url: galleryData.data.data[i].path,
+              public_id: galleryData.data.data[i].filename,
+            });
+          }
+
+          galleryData.data.acknowledgement
+            ? dispatch(setGallery(galleryInformation))
+            : console.log(galleryData);
         } catch (error) {
           console.log(error);
         }
@@ -29,4 +60,9 @@ const uploadApi = apiSlice.injectEndpoints({
   }),
 });
 
-export const { useUploadUserAvatarMutation } = uploadApi;
+export const { useUploadPhotoMutation, useUploadGalleryMutation } = uploadApi;
+
+/**
+ * Uploading multiple files via formData using JavaScript
+ * https://www.freecodecamp.org/news/formdata-explained/
+ */
