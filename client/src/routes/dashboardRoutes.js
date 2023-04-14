7d06e4f020639005d5324843377992a135e4cc6b
hasibulislam999 <hasib143sl@gmail.com>
2023-04-14 22:12:44 +0600

    integrate split code routing and routes

--- client/src/routes/dashboardRoutes.js
+++ client/src/routes/dashboardRoutes.js
@@ -1,23 +1,36 @@
 import Dashboard from "../layouts/dashboard/Dashboard";
 import AddNewProduct from "../pages/dashboard/AddNewProduct";
 import Analytics from "../pages/dashboard/Analytics";
+import ListProduct from "../pages/dashboard/ListProduct";
+import UpdateProduct from "../pages/dashboard/UpdateProduct";
 import PrivateRoute from "../utils/PrivateRoutes";
+import SplitRouter from "./SplitRouter";
 
 const dashboardRoutes = {
   path: "/dashboard",
   element: (
     <PrivateRoute>
-      <Dashboard />
+      <SplitRouter>
+        <Dashboard />
+      </SplitRouter>
     </PrivateRoute>
   ),
   children: [
     {
       path: "/dashboard",
-      element: <Analytics />,
+      element: <SplitRouter>{<Analytics />}</SplitRouter>,
     },
     {
       path: "add-new-product",
-      element: <AddNewProduct />,
+      element: <SplitRouter>{<AddNewProduct />}</SplitRouter>,
+    },
+    {
+      path: "list-product",
+      element: <SplitRouter>{<ListProduct />}</SplitRouter>,
+    },
+    {
+      path: "update-product/:pid",
+      element: <SplitRouter>{<UpdateProduct />}</SplitRouter>,
     },
   ],
 };
