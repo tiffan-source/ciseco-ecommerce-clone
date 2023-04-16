58b8f221b151121230bd6409458596379ed2cd9d
hasibulislam999 <hasib143sl@gmail.com>
2023-04-16 20:50:54 +0600

    new side bar routes init

--- client/src/layouts/dashboard/dashboardRoutes.js
+++ client/src/layouts/dashboard/dashboardRoutes.js
@@ -2,18 +2,28 @@ const iconStyle = "lg:h-4 md:h-10 h-8 lg:w-4 md:w-10 w-8 lg:mx-0 md:mx-auto";
 
 const dashboardRoutes = [
   {
-    icon: <CartPlus className={iconStyle} />,
+    icon: <Product className={iconStyle} />,
     name: "Add New Product",
     anchor: "add-new-product",
   },
   {
-    icon: <ListProduct className={iconStyle} />,
+    icon: <List className={iconStyle} />,
     name: "List Product",
     anchor: "list-product",
   },
+  {
+    icon: <Category className={iconStyle} />,
+    name: "Add New Category",
+    anchor: "add-new-category",
+  },
+  {
+    icon: <List className={iconStyle} />,
+    name: "List Category",
+    anchor: "list-category",
+  },
 ];
 
-function CartPlus(props) {
+function Product(props) {
   return (
     <svg
       {...props}
@@ -30,7 +40,7 @@ function CartPlus(props) {
   );
 }
 
-function ListProduct(props) {
+function Category(props) {
   return (
     <svg
       {...props}
@@ -38,13 +48,31 @@ function ListProduct(props) {
       width="16"
       height="16"
       fill="currentColor"
-      className="bi bi-list-nested"
+      className="bi bi-stack"
+      viewBox="0 0 16 16"
+    >
+      <path d="m14.12 10.163 1.715.858c.22.11.22.424 0 .534L8.267 15.34a.598.598 0 0 1-.534 0L.165 11.555a.299.299 0 0 1 0-.534l1.716-.858 5.317 2.659c.505.252 1.1.252 1.604 0l5.317-2.66zM7.733.063a.598.598 0 0 1 .534 0l7.568 3.784a.3.3 0 0 1 0 .535L8.267 8.165a.598.598 0 0 1-.534 0L.165 4.382a.299.299 0 0 1 0-.535L7.733.063z" />
+      <path d="m14.12 6.576 1.715.858c.22.11.22.424 0 .534l-7.568 3.784a.598.598 0 0 1-.534 0L.165 7.968a.299.299 0 0 1 0-.534l1.716-.858 5.317 2.659c.505.252 1.1.252 1.604 0l5.317-2.659z" />
+    </svg>
+  );
+}
+
+function List(porps) {
+  return (
+    <svg
+      {...porps}
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="16"
+      fill="currentColor"
+      className="bi bi-list-stars"
       viewBox="0 0 16 16"
     >
       <path
         fillRule="evenodd"
-        d="M4.5 11.5A.5.5 0 0 1 5 11h10a.5.5 0 0 1 0 1H5a.5.5 0 0 1-.5-.5zm-2-4A.5.5 0 0 1 3 7h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm-2-4A.5.5 0 0 1 1 3h10a.5.5 0 0 1 0 1H1a.5.5 0 0 1-.5-.5z"
+        d="M5 11.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5z"
       />
+      <path d="M2.242 2.194a.27.27 0 0 1 .516 0l.162.53c.035.115.14.194.258.194h.551c.259 0 .37.333.164.493l-.468.363a.277.277 0 0 0-.094.3l.173.569c.078.256-.213.462-.423.3l-.417-.324a.267.267 0 0 0-.328 0l-.417.323c-.21.163-.5-.043-.423-.299l.173-.57a.277.277 0 0 0-.094-.299l-.468-.363c-.206-.16-.095-.493.164-.493h.55a.271.271 0 0 0 .259-.194l.162-.53zm0 4a.27.27 0 0 1 .516 0l.162.53c.035.115.14.194.258.194h.551c.259 0 .37.333.164.493l-.468.363a.277.277 0 0 0-.094.3l.173.569c.078.255-.213.462-.423.3l-.417-.324a.267.267 0 0 0-.328 0l-.417.323c-.21.163-.5-.043-.423-.299l.173-.57a.277.277 0 0 0-.094-.299l-.468-.363c-.206-.16-.095-.493.164-.493h.55a.271.271 0 0 0 .259-.194l.162-.53zm0 4a.27.27 0 0 1 .516 0l.162.53c.035.115.14.194.258.194h.551c.259 0 .37.333.164.493l-.468.363a.277.277 0 0 0-.094.3l.173.569c.078.255-.213.462-.423.3l-.417-.324a.267.267 0 0 0-.328 0l-.417.323c-.21.163-.5-.043-.423-.299l.173-.57a.277.277 0 0 0-.094-.299l-.468-.363c-.206-.16-.095-.493.164-.493h.55a.271.271 0 0 0 .259-.194l.162-.53z" />
     </svg>
   );
 }
