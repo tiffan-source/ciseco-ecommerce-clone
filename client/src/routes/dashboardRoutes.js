9eb244976bd83854729a5264e7284ee62cc38f2b
hasibulislam999 <hasib143sl@gmail.com>
2023-04-16 20:52:47 +0600

    new route integrated

--- client/src/routes/dashboardRoutes.js
+++ client/src/routes/dashboardRoutes.js
@@ -5,6 +5,9 @@ import UpdateProduct from "../pages/dashboard/product/UpdateProduct";
 import PrivateRoute from "../utils/PrivateRoutes";
 import SplitRouter from "./SplitRouter";
 import ListProduct from "../pages/dashboard/product/ListProduct";
+import AddNewCategory from "../pages/dashboard/category/AddNewCategory";
+import ListCategory from "../pages/dashboard/category/ListCategory";
+import UpdateCategory from "../pages/dashboard/category/UpdateCategory";
 
 const dashboardRoutes = {
   path: "/dashboard",
@@ -31,6 +34,18 @@ const dashboardRoutes = {
     {
       path: "update-product/:pid",
       element: <SplitRouter>{<UpdateProduct />}</SplitRouter>,
+    },
+    {
+      path: "add-new-category",
+      element: <SplitRouter>{<AddNewCategory />}</SplitRouter>,
+    },
+    {
+      path: "list-category",
+      element: <SplitRouter>{<ListCategory />}</SplitRouter>,
+    },
+    {
+      path: "update-category/:cid",
+      element: <SplitRouter>{<UpdateCategory />}</SplitRouter>,
     },
   ],
 };
