856efa67121292128553672178bdb2f2f04ca124
hasibulislam999 <hasib143sl@gmail.com>
2023-04-16 20:52:13 +0600

    refactor sourcing init

--- client/src/pages/dashboard/product/AddNewProduct.js
+++ client/src/pages/dashboard/product/AddNewProduct.js
@@ -18,10 +18,8 @@ const AddNewProduct = () => {
   } = useForm();
 
   // server side credentials
-  const [
-    createProduct,
-    { isLoading: productCreating},
-  ] = useCreateProductMutation();
+  const [createProduct, { isLoading: productCreating }] =
+    useCreateProductMutation();
   const [uploadProductThumbnail, { isLoading: thumbnailUploading }] =
     useUploadPhotoMutation();
   const [uploadGallery, { isLoading: galleryUploading }] =
@@ -332,7 +330,7 @@ const AddNewProduct = () => {
                         <div role="status">
                           <svg
                             aria-hidden="true"
-                            class="w-5 h-5 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                            className="w-5 h-5 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
                             viewBox="0 0 100 101"
                             fill="none"
                             xmlns="http://www.w3.org/2000/svg"
@@ -346,7 +344,7 @@ const AddNewProduct = () => {
                               fill="currentFill"
                             />
                           </svg>
-                          <span class="sr-only">Loading...</span>
+                          <span className="sr-only">Loading...</span>
                         </div>
                         Thumbnail uploading
                       </span>
@@ -356,13 +354,13 @@ const AddNewProduct = () => {
                       <span className="flex">
                         <svg
                           aria-hidden="true"
-                          class="w-5 h-5 mr-1.5 text-green-500 dark:text-green-400 flex-shrink-0"
+                          className="w-5 h-5 mr-1.5 text-green-500 dark:text-green-400 flex-shrink-0"
                           fill="currentColor"
                           viewBox="0 0 20 20"
                           xmlns="http://www.w3.org/2000/svg"
                         >
                           <path
-                            fill-rule="evenodd"
+                            fillRule="evenodd"
                             d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
                             clip-rule="evenodd"
                           ></path>
@@ -427,7 +425,7 @@ const AddNewProduct = () => {
                         <div role="status">
                           <svg
                             aria-hidden="true"
-                            class="w-5 h-5 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                            className="w-5 h-5 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
                             viewBox="0 0 100 101"
                             fill="none"
                             xmlns="http://www.w3.org/2000/svg"
@@ -441,7 +439,7 @@ const AddNewProduct = () => {
                               fill="currentFill"
                             />
                           </svg>
-                          <span class="sr-only">Loading...</span>
+                          <span className="sr-only">Loading...</span>
                         </div>
                         Gallery uploading
                       </span>
@@ -451,13 +449,13 @@ const AddNewProduct = () => {
                       <span className="flex">
                         <svg
                           aria-hidden="true"
-                          class="w-5 h-5 mr-1.5 text-green-500 dark:text-green-400 flex-shrink-0"
+                          className="w-5 h-5 mr-1.5 text-green-500 dark:text-green-400 flex-shrink-0"
                           fill="currentColor"
                           viewBox="0 0 20 20"
                           xmlns="http://www.w3.org/2000/svg"
                         >
                           <path
-                            fill-rule="evenodd"
+                            fillRule="evenodd"
                             d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
                             clip-rule="evenodd"
                           ></path>
@@ -510,7 +508,7 @@ const AddNewProduct = () => {
                 <svg
                   aria-hidden="true"
                   role="status"
-                  class="inline w-4 h-4 mr-3 text-white animate-spin"
+                  className="inline w-4 h-4 mr-3 text-white animate-spin"
                   viewBox="0 0 100 101"
                   fill="none"
                   xmlns="http://www.w3.org/2000/svg"
