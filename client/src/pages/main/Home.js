7c985bc2bf49ce88c5af370b0e0fefc2c4404a1d
hasibulislam999 <hasib143sl@gmail.com>
2023-04-18 14:25:05 +0600

    some components init

--- client/src/pages/main/Home.js
+++ client/src/pages/main/Home.js
@@ -2,14 +2,34 @@ import React from "react";
 import Banner from "../../components/home/Banner";
 import BuyingSteps from "../../components/home/BuyingSteps";
 import NewArrivals from "../../components/home/NewArrivals";
+import { useDisplayProductsQuery } from "../../features/product/productApi";
+import KidsProduct from "../../components/home/KidsProduct";
+import ExpertChoice from "../../components/home/ExpertChoice";
 
 const Home = () => {
+  const { data: productData, isLoading } = useDisplayProductsQuery({
+    page: 0,
+    limit: 0,
+  });
+
+  const products = productData?.data || [];
+
   return (
     <section className="container mx-auto flex flex-col gap-y-32 px-4">
       <Banner />
-      <div className="px-32 flex flex-col gap-y-32">
+      <div className="lg:px-32 flex flex-col gap-y-32">
         <BuyingSteps />
-        <NewArrivals />
+        <NewArrivals
+          products={products.slice(-4)}
+          loading={isLoading}
+          type={"carousel"}
+        />
+        <KidsProduct />
+        <ExpertChoice
+          products={products.slice(0, 4)}
+          loading={isLoading}
+          type={"slide"}
+        />
       </div>
     </section>
   );
