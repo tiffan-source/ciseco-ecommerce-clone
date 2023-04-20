b518341e9f1b9d0f95dc1ea928d7f0a93503e6f8
hasibulislam999 <hasib143sl@gmail.com>
2023-04-20 12:19:22 +0600

    visible to top

--- client/src/components/main/CheckoutModal.js
+++ client/src/components/main/CheckoutModal.js
@@ -11,7 +11,7 @@ const CheckoutModal = ({ isOpen, setIsOpen }) => {
       <div className="fixed inset-0 flex items-center justify-center"></div>
 
       <Transition appear show={isOpen} as={Fragment}>
-        <Dialog as="div" className="relative z-10" onClose={closeModal}>
+        <Dialog as="div" className="relative z-50" onClose={closeModal}>
           <Transition.Child
             as={Fragment}
             enter="ease-out duration-300"
@@ -24,7 +24,7 @@ const CheckoutModal = ({ isOpen, setIsOpen }) => {
             <div className="fixed inset-0 bg-black bg-opacity-25" />
           </Transition.Child>
 
-          <div className="fixed inset-0 overflow-y-auto">
+          <div className="fixed inset-0 overflow-y-auto z-50">
             <div className="flex min-h-full items-center justify-center p-4 text-center">
               <Transition.Child
                 as={Fragment}
